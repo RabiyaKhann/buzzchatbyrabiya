6b359b4cbdc0cd575efc639322fa0431b5c24f85
RabiyaKhann <90935572+RabiyaKhann@users.noreply.github.com>
2024-07-04 22:01:54 +0530

    Update next.config.cjs

--- next.config.cjs
+++ next.config.cjs
@@ -1,19 +1,22 @@
 const nextConfig = {
 	webpack: (config, { isServer }) => {
 	  config.module.rules.push({
-		test: js$,
-		include: /convex/_generated,
+		test: /\.js$/,
+		include: /convex\/_generated/,
 		type: 'javascript/esm',
 	  });
   
 	  return config;
 	},
 	images: {
-	  remotePatterns: [
-		{ hostname: "limitless-porpoise-331.convex.cloud" },
-		{ hostname: "oaidalleapiprodscus.blob.core.windows.net" },
-	  ],
-	},
+		remotePatterns: [
+		  {
+			protocol: 'https',
+			hostname: 'limitless-porpoise-331.convex.cloud',
+			pathname: '/api/storage/**',  // Match the path structure
+		  },
+		],
+	  },
   };
   
   module.exports = nextConfig;
